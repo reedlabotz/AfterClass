b0d703a213f446b8137d95e450f9832e233347b0
Reed La Botz <reedlabotz@gmail.com>
2012-11-16 13:38:22 -0600

    Better availability selector code.

--- app/social/views.py
+++ app/social/views.py
@@ -40,8 +40,8 @@ def first_time_account(request):
 
 def first_time_availability(request):
    user_availability_form = UserAvailabilityForm(instance=request.user.get_profile())
-   days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
-   hours = ['8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm','9pm','10pm','11pm']
+   days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+   hours = ['12am','1am','2am','3am','4am','5am','6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm','9pm','10pm','11pm']
 
    if request.POST:
       user_availability_form = UserAvailabilityForm(request.POST,instance=request.user.get_profile())
